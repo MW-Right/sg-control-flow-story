abab97646e4a17a8df244730717a29d3e428c5e4
Michael Wright <mwright@spartaglobal.com>
2019-01-14 11:18:29 +0000

    Removed the second chapter page, added second chapter content

--- js/story.js
+++ js/story.js
@@ -1,3 +1,4 @@
+// DECLARING KNOWN CONSTANTS
 var workStart = 9.5;
 var leaveTime;
 var firstTrain1 = 445 / 60;
@@ -13,11 +14,12 @@ var leaveTime;
 var timeUntilTrain;
 var missTrain;
 
-
+// PROGRAM INITIATION
 var person = prompt("Please enter your name");
 var title = person + "'s Getting to Work on Time";
 document.getElementById("title").innerHTML = title;
 
+// DRIVING TO THE STATION QUESTION
 function question1() {
     leaveTime = prompt("What time do you want to leave?");
     var hoursMinutes = leaveTime.split(":");
@@ -37,7 +39,23 @@ function question1() {
     } else {
         missTrain = "There was a 5 minute delay due to traffic but you still made it to the train on time!";
         currentTime = firstTrain1Arrives;
-    }
+    };
     document.getElementById("missTrain").innerHTML = missTrain;
     console.log(currentTime);
+};
+
+// SECTION 2
+function nextChapter() {
+    document.getElementById("chapter2").innerHTML = "<h2>Chapter 2 - The Second Train</h2>" +
+    "<p>You managed to make it to Feltham at " + currentTime + ". Must be pretty tired</p>" + "<p>There's a coffee shop just outside the station. Do you want to grab a coffee?</p>" + "<button type=\"button\" onclick=\"question2()\">Question 2</button>";
+};
+var coffee;
+function question2() {
+    coffee = prompt("Do you go get a coffee?");
+    if (coffee == "yes") {
+        document.getElementById("getACoffee").innerHTML = "The lady at the till says you look tired and gives you a free coffee and breakfast toastie. It takes you 8 mins, but your train was delayed so you make it on time. You get on the train buzzing on caffiene.";
+    } else {
+        document.getElementById("getACoffee").innerHTML = "You sit and wait for the train. It is then delayed by a further 5 mins. You get on the train half asleep.";
+    };
+    document.getElementById("afterCoffee").innerHTML = "<p>You get to the station at 9:05, you have 25 mins to get to work</p>"; 
 };
